f363449fc133d26944038ed8377b91401e9320ec
Evgeny Boyko <evgeny.boyko@nordpoolgroup.com>
2018-11-18 19:19:39 +0200

    structure refactor

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 var express = require('express');
 var app = express();
 var db = require('./app/dbConfig/db');
+var path = require('path');
+
 
 var OrdersController = require('./app/controllers/ordersController');
 app.use('/', OrdersController);
-
+app.use(express.static(path.join(__dirname, 'public')));
 module.exports = app;
